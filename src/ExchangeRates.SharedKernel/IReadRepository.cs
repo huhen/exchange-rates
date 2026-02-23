1809a95f570bcd97cf45990bc036b985d70ed358
huhen <9335143+huhen@users.noreply.github.com>
2026-02-23 16:19:27 +0300

    Add repo interfaces

---
+++ src/ExchangeRates.SharedKernel/IReadRepository.cs
@@ -0,0 +1,5 @@
+using Ardalis.Specification;
+
+namespace ExchangeRates.SharedKernel;
+
+public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot;
---
+++ src/ExchangeRates.SharedKernel/IRepository.cs
@@ -0,0 +1,5 @@
+using Ardalis.Specification;
+
+namespace ExchangeRates.SharedKernel;
+
+public interface IRepository<T> : IRepositoryBase<T> where T : class, IAggregateRoot;
